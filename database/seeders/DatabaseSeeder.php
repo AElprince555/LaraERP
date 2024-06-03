04d978a1b70011a938fdced418361fed3e14b04e
Ahmed El prince <102730608+AElprince555@users.noreply.github.com>
2024-06-03 23:39:32 +0300

    update seeders data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\General\World\Country;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Seeders\General\World\CitySeeder;
+use Database\Seeders\General\World\CountrySeeder;
+use Database\Seeders\General\World\CurrencySeeder;
+use Database\Seeders\General\World\PeriodSeeder;
+use Database\Seeders\General\World\StateSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +22,16 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Ahmed Mahmoud',
+            'email' => 'admin@laraerp.com',
+            'password' => '01011263555'
+        ]);
+        $this->call([
+            CountrySeeder::class,
+            StateSeeder::class,
+            CitySeeder::class,
+            PeriodSeeder::class,
+            CurrencySeeder::class
         ]);
     }
 }
